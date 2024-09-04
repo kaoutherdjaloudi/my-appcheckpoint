9d6602b07814209243fd10212637d8d0438bd28d
kaoutherdjaloudi <155756857+kaoutherdjaloudi@users.noreply.github.com>
2024-09-04 18:36:25 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Greeting from './Greeting';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div>
+      <Greeting name="Kaouther" />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
